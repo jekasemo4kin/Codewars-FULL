58248a6838f41ae4949a036d3db8445103d4ee2a
jekasemo4kin <jekasemo4kin@gmail.com>
2022-04-05 01:18:25 +0300

    codewars-10

---
+++ war.js
@@ -0,0 +1,13 @@
+function head(x){
+
+    return +(x.slice(0,1))
+    }
+    function tail(x){
+    return (x.slice(1))
+    }
+    function init(x){
+    return (x.slice(0,arr.length-1))
+    }
+    function last(x){
+    return +(x.slice(arr.length-1))
+    }
